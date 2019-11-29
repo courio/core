36014cefa156333cd8cde81a240d3899462f5f13
Matt Hicks <matt@outr.com>
2019-11-29 08:26:15 -0600

    Added id generation for OrganizationPreview

--- core/src/main/scala/io/cour/model/OrganizationPreview.scala
+++ core/src/main/scala/io/cour/model/OrganizationPreview.scala
@@ -1,5 +1,10 @@
 package io.cour.model
 
 import com.outr.arango.Id
+import io.youi.Unique
 
 case class OrganizationPreview(id: Id[OrganizationPreview], name: String)
+
+object OrganizationPreview {
+  def id(value: String = Unique()): Id[OrganizationPreview] = Id(value, "organizations")
+}
