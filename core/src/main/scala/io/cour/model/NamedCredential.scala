ecc4b077a913a83289320dceb9d38b36203b46b1
Matt Hicks <matt@outr.com>
2019-11-29 17:16:52 -0600

    Added organization to MessagePreview and updated NamedCredential to include lastModified and generates URLs

--- core/src/main/scala/io/cour/model/NamedCredential.scala
+++ core/src/main/scala/io/cour/model/NamedCredential.scala
@@ -6,5 +6,7 @@ import io.youi.net.URL
 case class NamedCredential[T](id: Id[T],
                               username: String,
                               name: Option[String],
-                              icon: URL,
-                              profile: URL)
+                              lastModified: Long) {
+  lazy val icon: URL = URL(s"https://s3.us-west-1.wasabisys.com/courio/user/${id.value}-icon.png?m=$lastModified")
+  lazy val profile: URL = URL(s"https://s3.us-west-1.wasabisys.com/courio/user/${id.value}-profile.png?m=$lastModified")
+}
