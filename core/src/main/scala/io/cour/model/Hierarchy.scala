65b049062c5733c458b6673c3e4f82c5314948bd
Matt Hicks <matt@outr.com>
2019-12-20 07:42:25 -0600

    Added convenience Hierarchy.unread to easily determine the unread count

--- core/src/main/scala/io/cour/model/Hierarchy.scala
+++ core/src/main/scala/io/cour/model/Hierarchy.scala
@@ -8,6 +8,7 @@ case class Hierarchy(organizations: List[OrganizationPreview],
   lazy val organizationById: Map[Id[OrganizationPreview], OrganizationPreview] = organizations.map(o => o.id -> o).toMap
   lazy val sourceById: Map[Id[SourcePreview], SourcePreview] = sources.map(s => s.id -> s).toMap
   lazy val streamById: Map[Id[StreamPreview], StreamPreview] = streams.map(s => s._id -> s).toMap
+  lazy val unread: Int = streams.map(_.unread).sum
   def sourcesByOrganization(id: Id[OrganizationPreview]): List[SourcePreview] = sources.filter(_.organizationId == id)
   def streamsBySource(id: Id[SourcePreview]): List[StreamPreview] = streams.filter(_.sourceId == id)
   def search(filter: String, limit: Int): List[StreamPreview] = {
