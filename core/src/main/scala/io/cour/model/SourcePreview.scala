e002b9c7193752cad47959e04de3ff4d87d5fa4d
Matt Hicks <matt@courio.com>
2020-04-07 20:04:54 -0500

    Minor updates

--- core/src/main/scala/io/cour/model/SourcePreview.scala
+++ core/src/main/scala/io/cour/model/SourcePreview.scala
@@ -1,5 +1,10 @@
 package io.cour.model
 
 import com.outr.arango.Id
+import io.youi.Unique
 
 case class SourcePreview(organizationId: Id[OrganizationPreview], id: Id[SourcePreview], name: String)
+
+object SourcePreview {
+  def id(value: String = Unique()): Id[SourcePreview] = Id(value, "sources")
+}
